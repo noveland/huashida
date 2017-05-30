dc101e50cb4e4289d035365c96bc50fd4cda13a1
2790788 <wang7147001@163.com>
2017-05-30 16:46:11 +0800

    项目开工
    首页基本完工，开始分页
    首页导航需要继续优化，移动端适配需要继续优化

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp');
+var rev = require('gulp-rev');
+var revReplace = require('gulp-rev-replace');
+var useref = require('gulp-useref');
+var filter = require('gulp-filter');
+var uglify = require('gulp-uglify');
+var csso = require('gulp-csso');
+gulp.task('default', function() {
+    var jsFilter = fliter('**/*.js',{restore:ture});
+    var cssFilter = fliter('**/*.css',{restore:ture});
+    var indexHtmlFilter = fliter(['**/*','!**/index.html'],{restore:ture});
+
+    return gulp.src('src/index.html')
+        .pipe(useref())
+        .pipe(jsFilter)
+        .pipe(uglify())
+        .pipe(jsFilter.restore)
+        .pipe(cssFilter)
+        .pipe(csso())
+        .pipe(cssFilter.restore)
+        .pipe(indexHtmlFilter)
+        .pipe(rev())
+        .pipe(indexHtmlFilter.restore)
+        .pipe(revReplace())
+        .pipe(gulp.dest('dist'));
+});
