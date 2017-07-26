c52b0302644323a6baef9adcfff453cfb2f27192
2790788 <wang7147001@163.com>
2017-07-26 21:20:33 +0800

    添加百度统计代码
    rt

---
+++ src/js/baidu.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?0d163a3286f194d4af62b407935649ef";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
